ac061670a856b5e7786a15e04a9e658eb60b1c9b
wachira <wachiraa954@gmail.com>
2022-10-05 11:18:04 +0300

    done

--- app/models/restuarant_pizza.rb
+++ app/models/restuarant_pizza.rb
@@ -1,2 +1,6 @@
-class RestuarantPizza < ApplicationRecord
+class RestaurantPizza < ApplicationRecord
+    belongs_to :pizza 
+    belongs_to :restaurant
+
+    validates :price, inclusion: 1..30
 end
